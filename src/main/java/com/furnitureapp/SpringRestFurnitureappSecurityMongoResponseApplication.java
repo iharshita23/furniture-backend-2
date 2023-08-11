6addf7a2ab4bdeb81f9b7b31f2f332627e35b5d7
HARSHITA VISHWAKARMA <72926293+iharshita23@users.noreply.github.com>
2023-08-11 12:25:52 +0530

    Update SpringRestFurnitureappSecurityMongoResponseApplication.java

--- src/main/java/com/furnitureapp/SpringRestFurnitureappSecurityMongoResponseApplication.java
+++ src/main/java/com/furnitureapp/SpringRestFurnitureappSecurityMongoResponseApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = {"com.furnitureapp.*"})
 public class SpringRestFurnitureappSecurityMongoResponseApplication {
 
 	public static void main(String[] args) {
